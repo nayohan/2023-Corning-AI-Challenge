80964030d27f931be18f732f90fbe6e6f0d4ec75
Myngsoo <myngsoo@hanyang.ac.kr>
2023-11-10 19:31:13 +0900

    eval_retriever

---
+++ retriever/run.sh
@@ -0,0 +1,18 @@
+# python test.py \
+#     --model_name_or_path bert-base-uncased \
+#     --evaluation_file THUDM/webglm-qa \
+#     --data_type test \
+#     --gpu_id 0 \
+#     --top_k 10 \
+#     --use_fast_tokenizer
+
+
+python test.py \
+    --model_name_or_path bert-base-uncased \
+    --simcse_model_name_or_path "princeton-nlp/sup-simcse-bert-base-uncased"\
+    --evaluation_file THUDM/webglm-qa \
+    --data_type test \
+    --do_simcse \
+    --gpu_id 0 \
+    --top_k 10 \
+    --use_fast_tokenizer
---
+++ retriever/utils.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+
+class Pooler(nn.Module):
+    
+    def __init__(self, pooler_type):
+        super().__init__()
+        self.pooler_type = pooler_type
+        assert self.pooler_type in ["cls", "cls_before_pooler", "max", "avg", "avg_top2", "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
+
+    def forward(self, attention_mask, outputs):
+        last_hidden = outputs.last_hidden_state
+        pooler_output = outputs.pooler_output
+        hidden_states = outputs.hidden_states
+
+        if self.pooler_type in ['cls_before_pooler', 'cls']:
+            return last_hidden[:, 0]
+        elif self.pooler_type == "avg":
+            return ((last_hidden * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1))
+        elif self.pooler_type == "max":
+            input_mask_expanded = (
+                attention_mask.unsqueeze(-1).expand(last_hidden.size()).float()
+            )
+            last_hidden[input_mask_expanded == 0] = -1e9
+            max_over_time = torch.max(last_hidden, 1)[0]
+            return max_over_time
+        elif self.pooler_type == "avg_first_last":
+            first_hidden = hidden_states[1]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((first_hidden + last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        elif self.pooler_type == "avg_top2":
+            second_last_hidden = hidden_states[-2]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((last_hidden + second_last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        else:
+            raise NotImplementedError
+        
+class Similarity(nn.Module):
+    
+    def __init__(self, temp):
+        super().__init__()
+        self.temp = temp
+        self.cos = nn.CosineSimilarity(dim=-1)
+
+    def forward(self, x, y):
+        return self.cos(x, y) / self.temp
+    
