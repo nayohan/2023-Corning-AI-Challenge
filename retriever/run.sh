66d09e05d7f8d083883cef4bb4fbc43315fc903e
Myngsooo <121186954+Hanyang-DILAB@users.noreply.github.com>
2023-11-11 14:46:45 +0900

    Update run.sh

--- retriever/run.sh
+++ retriever/run.sh
@@ -1,5 +1,6 @@
 # python test.py \
 #     --model_name_or_path bert-base-uncased \
+#     --simcse_model_name_or_path "princeton-nlp/sup-simcse-bert-base-uncased"\
 #     --evaluation_file THUDM/webglm-qa \
 #     --data_type test \
 #     --gpu_id 0 \
