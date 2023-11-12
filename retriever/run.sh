520f624695e16f58831dff281ae11ce71415ac4a
Myngsoo <myngsoo@hanyang.ac.kr>
2023-11-12 06:43:43 +0000

    run.sh_update

--- retriever/run.sh
+++ retriever/run.sh
@@ -1,19 +1,20 @@
+python test.py \
+    --model_name_or_path princeton-nlp/sup-simcse-bert-base-uncased \
+    --evaluation_file THUDM/webglm-qa \
+    --data_type validation \
+    --pooler_type cls \
+    --max_length 64 \
+    --gpu_id 0 \
+    --top_k 1 \
+    --use_fast_tokenizer
+
+
 # python test.py \
 #     --model_name_or_path bert-base-uncased \
 #     --simcse_model_name_or_path "princeton-nlp/sup-simcse-bert-base-uncased"\
 #     --evaluation_file THUDM/webglm-qa \
-#     --data_type test \
+#     --data_type validation \
+#     --do_simcse \
 #     --gpu_id 0 \
-#     --top_k 10 \
+#     --top_k 1 \
 #     --use_fast_tokenizer
-
-
-python test.py \
-    --model_name_or_path bert-base-uncased \
-    --simcse_model_name_or_path "princeton-nlp/sup-simcse-bert-base-uncased"\
-    --evaluation_file THUDM/webglm-qa \
-    --data_type test \
-    --do_simcse \
-    --gpu_id 0 \
-    --top_k 10 \
-    --use_fast_tokenizer
