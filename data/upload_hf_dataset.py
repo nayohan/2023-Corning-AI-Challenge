10143a4ee0f6ab8f00f02153df8003954e0c5446
myngsooo <hms8717@naver.com>
2024-02-12 03:39:13 +0900

    [ADD] upload image

--- data/upload_hf_dataset.py
+++ data/upload_hf_dataset.py
@@ -2,14 +2,13 @@
 import pandas as pd
 from datasets import load_dataset, Dataset, DatasetDict
 
-train_df = pd.read_json('/home/uj-user/Yo/ClosedAI/data/tasks/DocQA7_MS/train.jsonl', lines=True, orient='records')
-valid_df = pd.read_json('/home/uj-user/Yo/ClosedAI/data/tasks/DocQA7_MS/valid.jsonl', lines=True, orient='records')
-test_df = pd.read_json('/home/uj-user/Yo/ClosedAI/data/tasks/DocQA7_MS/test.jsonl', lines=True, orient='records')
+train_df = pd.read_json('/home/user/ClosedAI/data/tasks/DocQA7_MS/train.jsonl', lines=True, orient='records')
+valid_df = pd.read_json('/home/user/ClosedAI/data/tasks/DocQA7_MS/valid.jsonl', lines=True, orient='records')
+test_df = pd.read_json('/home/user/ClosedAI/data/tasks/DocQA7_MS/test.jsonl', lines=True, orient='records')
 
 train = Dataset.from_pandas(train_df)
 valid = Dataset.from_pandas(valid_df)
 test = Dataset.from_pandas(test_df)
 
 DocQA7 = DatasetDict({'train':train, 'validation':valid, 'test':test})
-print(DocQA7)
-DocQA7.push_to_hub('nayohan/CorningAI-DocQA')
+DocQA7.push_to_hub('myngsoooo/CorningAI-DocQA')
